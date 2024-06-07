c4ee5311ecdcdc0b2e4cd6e64e1c926caffa3764
Nash Stewart <nashkstewart@gmail.com>
2024-06-06 21:26:21 -0600

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-caesar-cipher
+
+My implementation of a [Ceasar Cipher](https://en.wikipedia.org/wiki/Caesar_cipher).
+This comes from [The Odien Project](https://www.theodinproject.com/lessons/ruby-caesar-cipher)
--- main.rb
+++ main.rb
@@ -4,15 +4,18 @@ phrase = gets.chomp
 puts "\nEnter a shift number"
 shift = gets.chomp.to_i
 
+# # # # # # #
+#  Cipher   #
+# # # # # # #
 phrase_bytes = phrase.bytes
-ciphered_phrase = phrase_bytes.map do |n|
-  if n.between?(97, 122)
+ciphered_phrase_array = phrase_bytes.map do |n|
+  if n.between?(97, 122) # Lowercase letters
     n = n + shift
     if n > 122
       new_shift = n - 122
       n = 96 + new_shift
     end
-  elsif n.between?(65, 90)
+  elsif n.between?(65, 90) # Uppercase letters
     n = n + shift
     if n > 90
       new_shift = n - 90
@@ -20,19 +23,23 @@ ciphered_phrase = phrase_bytes.map do |n|
     end
   end
   n.chr
-end.join
+end
+ciphered_phrase = ciphered_phrase_array.join
 
 puts "\nCiphered phrase\n#{ciphered_phrase}"
 
+# # # # # # # #
+#  Un-cipher  #
+# # # # # # # #
 ciphered_bytes = ciphered_phrase.bytes
-original_phrase = ciphered_bytes.map do |n|
-  if n.between?(97, 122)
+original_phrase_array = ciphered_bytes.map do |n|
+  if n.between?(97, 122) # Lowercase letters
     n = n - shift
     if n < 97
       new_shift = 97 - n
       n = 123 - new_shift
     end
-  elsif n.between?(65, 90)
+  elsif n.between?(65, 90) # Uppercase letters
     n = n - shift
     if n < 65
       new_shift = 65 - n
@@ -40,7 +47,8 @@ original_phrase = ciphered_bytes.map do |n|
     end
   end
   n.chr
-end.join
+end
+original_phrase = original_phrase_array.join
 
 puts "\nDe-ciphered phrase\n#{original_phrase}"
 
