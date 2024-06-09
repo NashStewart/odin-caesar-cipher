834cbd5203ded59b35e5d0b704970a6425123600
Nash Stewart <nashkstewart@gmail.com>
2024-06-09 12:09:21 -0600

    Add Gemfile and rubocop

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 puts 'Enter a phrase to cipher'
 phrase = gets.chomp
 
@@ -10,13 +12,13 @@ shift = gets.chomp.to_i
 phrase_bytes = phrase.bytes
 ciphered_phrase_array = phrase_bytes.map do |n|
   if n.between?(97, 122) # Lowercase letters
-    n = n + shift
+    n += shift
     if n > 122
       new_shift = n - 122
       n = 96 + new_shift
     end
   elsif n.between?(65, 90) # Uppercase letters
-    n = n + shift
+    n += shift
     if n > 90
       new_shift = n - 90
       n = 64 + new_shift
@@ -34,13 +36,13 @@ puts "\nCiphered phrase\n#{ciphered_phrase}"
 ciphered_bytes = ciphered_phrase.bytes
 original_phrase_array = ciphered_bytes.map do |n|
   if n.between?(97, 122) # Lowercase letters
-    n = n - shift
+    n -= shift
     if n < 97
       new_shift = 97 - n
       n = 123 - new_shift
     end
   elsif n.between?(65, 90) # Uppercase letters
-    n = n - shift
+    n -= shift
     if n < 65
       new_shift = 65 - n
       n = 91 - new_shift
@@ -51,4 +53,3 @@ end
 original_phrase = original_phrase_array.join
 
 puts "\nDe-ciphered phrase\n#{original_phrase}"
-
